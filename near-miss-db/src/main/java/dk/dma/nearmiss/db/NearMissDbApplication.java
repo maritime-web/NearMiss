6c6a9e52b26f1f7e0aa19da8bc282f3d7982cca9
Klaus Kirkelund Andersen <klaus1@strateg.dk>
2018-10-14 13:35:42 +0200

    Added near-miss-db component.

---
+++ near-miss-db/src/main/java/dk/dma/nearmiss/db/NearMissDbApplication.java
@@ -0,0 +1,12 @@
+package dk.dma.nearmiss.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearMissDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearMissDbApplication.class, args);
+	}
+}
