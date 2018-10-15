d651acd2f27994ea510c1bfb28426447005a599a
Klaus Kirkelund Andersen <klaus1@strateg.dk>
2018-10-15 14:25:20 +0200

    Updated gps-simulator to use Spring Beans.

--- gps-simulator/src/main/java/dk/dma/nearmiss/gpssimulator/GpsReceiver.java
+++ gps-simulator/src/main/java/dk/dma/nearmiss/gpssimulator/GpsReceiver.java
@@ -2,17 +2,24 @@ package dk.dma.nearmiss.gpssimulator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GpsReceiver implements Observer {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     private final GpsSimulatorClient client;
 
     public GpsReceiver(GpsSimulatorClient client) {
         this.client = client;
+        client.addListener(this);
     }
 
     @Override
     public void update() {
         logger.info(String.format("Client has received: %s", client.getAnswer()));
+    }
+
+    public GpsSimulatorClient getClient() {
+        return client;
     }
 }
--- gps-simulator/src/main/java/dk/dma/nearmiss/gpssimulator/GpsSimulator.java
+++ gps-simulator/src/main/java/dk/dma/nearmiss/gpssimulator/GpsSimulator.java
@@ -2,12 +2,19 @@ package dk.dma.nearmiss.gpssimulator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 import static java.lang.Thread.sleep;
 
-public class Gps extends AbstractGps {
+/**
+ * This class is the actual GPS simulator.
+ * It will notify all listeners every given time period (currently once per second).
+ * Listeners are able to ask the GPS simulator of the current position.
+ */
+@Component
+public class GpsSimulator extends AbstractSubject implements Runnable {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     private String message;
 
@@ -24,7 +31,7 @@ public class Gps extends AbstractGps {
             }
     }
 
-    public String getMessage() {
+    String getMessage() {
         return message;
     }
 }
