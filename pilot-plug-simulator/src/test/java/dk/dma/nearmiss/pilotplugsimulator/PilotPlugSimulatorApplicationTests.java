99cca28b008ed5e8f36741ba5b89ceb3376be16c
Thomas Borg Salling <tbsalling@tbsalling.dk>
2018-10-16 08:11:38 +0200

    Disable @SpringBootTest temporarily

--- pilot-plug-simulator/src/test/java/dk/dma/nearmiss/pilotplugsimulator/PilotPlugSimulatorApplicationTests.java
+++ pilot-plug-simulator/src/test/java/dk/dma/nearmiss/pilotplugsimulator/PilotPlugSimulatorApplicationTests.java
@@ -2,11 +2,10 @@ package dk.dma.nearmiss.pilotplugsimulator;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest(webEnvironment = NONE)
 public class PilotPlugSimulatorApplicationTests {
 
 	@Test
