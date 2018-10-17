9d464aceb7701c7c94af76d1f5b97ec0375c42fc
Thomas Borg Salling <tbsalling@tbsalling.dk>
2018-10-17 13:22:04 +0200

    Add initial implementation and test of Ellipse

---
+++ near-miss-engine/src/main/java/dk/dma/nearmiss/engine/Ellipse.java
@@ -0,0 +1,70 @@
+package dk.dma.nearmiss.engine;
+
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.geom.util.AffineTransformation;
+import org.locationtech.jts.io.WKTWriter;
+import org.locationtech.jts.util.GeometricShapeFactory;
+
+import static java.lang.Math.PI;
+
+/**
+ * This class represents an Ellipse.
+ *
+ * The ellipse is intended to support geometric calculations of the surface of the Earth; but limited to distances
+ * at the surface where the Earth can the surface can safely be assumed to a linear, flat plane.
+ *
+ */
+public class Ellipse {
+
+    private final Polygon _internalRepresentation;
+
+    final static int METERS_PER_MINUTE_LATITUDE = 1852;
+    final static int MINUTES_PER_DEGREE_LATITUDE = 60;
+    final static int METERS_PER_DEGREE_LATITUDE = METERS_PER_MINUTE_LATITUDE * MINUTES_PER_DEGREE_LATITUDE;
+
+    final static int METERS_PER_MINUTE_LONGITUDE = 1000; // not precise - only valid around 55N
+    final static int MINUTES_PER_DEGREE_LONGITUDE = 60;
+    final static int METERS_PER_DEGREE_LONGITUDE = METERS_PER_MINUTE_LONGITUDE * MINUTES_PER_DEGREE_LONGITUDE;
+
+    /**
+     * Create an Ellipse
+     *
+     * @param centreLatitude Latitude of the ellipse's centre in degrees
+     * @param centreLongitude Longitude of the ellipse's center in degrees
+     * @param verticalSemiAxisBeforeRotationInMeters Length of the ellipse's vertical semi axis before rotation in meters
+     * @param horizontalSemiAxisBeforeRotationInMeters Length of the ellipse's horizontal semi axis before rotation in meters. The values supplied for this parameters are only valid in meters around latitude 55 degrees North or South.
+     * @param rotateDegreesCounterClockwise Counter-clockwise rotation angle of the ellipse in degrees
+     */
+    public Ellipse(double centreLatitude, double centreLongitude, double verticalSemiAxisBeforeRotationInMeters, double horizontalSemiAxisBeforeRotationInMeters, double rotateDegreesCounterClockwise) {
+        if (verticalSemiAxisBeforeRotationInMeters <= 0)
+            throw new IllegalArgumentException("verticalSemiAxisBeforeRotationInMeters must be a positive number");
+        if (horizontalSemiAxisBeforeRotationInMeters <= 0)
+            throw new IllegalArgumentException("horizontalSemiAxisBeforeRotationInMeters must be a positive number");
+
+        double vAxis = verticalSemiAxisBeforeRotationInMeters / METERS_PER_DEGREE_LATITUDE;
+        double hAxis = horizontalSemiAxisBeforeRotationInMeters / METERS_PER_DEGREE_LONGITUDE;
+        double orientRad = rotateDegreesCounterClockwise * (PI/180.0);
+
+        Coordinate centre = new Coordinate(centreLongitude, centreLatitude);
+        GeometricShapeFactory gsf = new GeometricShapeFactory();
+        gsf.setCentre(centre);
+        gsf.setWidth(hAxis);
+        gsf.setHeight(vAxis);
+        gsf.setNumPoints(100);
+        Polygon ellipse = gsf.createEllipse();
+        AffineTransformation trans = AffineTransformation.rotationInstance(orientRad, centre.x, centre.y);
+        ellipse.apply(trans);
+
+        this._internalRepresentation = ellipse;
+    }
+
+    public boolean intersects(Ellipse otherEllipse) {
+        return _internalRepresentation.intersects(otherEllipse._internalRepresentation);
+    }
+
+    public String toWkt() {
+        return new WKTWriter().write(_internalRepresentation);
+    }
+
+}
