ff3ea5b0c07e9243fbec9691bfd027639029b15b
Klaus Kirkelund Andersen <klaus1@strateg.dk>
2018-10-19 15:07:31 +0200

    Added hosting of h2 tcp me db to nearmiss-db (for multiple connections).

--- near-miss-db/src/main/java/dk/dma/nearmiss/db/NearMissDbApplication.java
+++ near-miss-db/src/main/java/dk/dma/nearmiss/db/NearMissDbApplication.java
@@ -1,12 +1,41 @@
 package dk.dma.nearmiss.db;
 
+import org.h2.tools.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
-public class NearMissDbApplication {
+public class NearMissDbApplication extends SpringBootServletInitializer {
+    static Logger logger = LoggerFactory.getLogger(NearMissDbApplication.class);
 
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
+		startH2Server();
 		SpringApplication.run(NearMissDbApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		startH2Server();
+		return application.sources(NearMissDbApplication.class);
+	}
+
+	private static void startH2Server() {
+        try {
+			Server h2Server = Server.createTcpServer().start();
+			if (h2Server.isRunning(true)) {
+                logger.info("H2 server was started and is running.");
+			} else {
+				throw new RuntimeException("Could not start H2 server.");
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException("Failed to start H2 server: ", e);
+		}
 	}
 }
