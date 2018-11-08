b94435fe604cfe80f67005c15ee77f5932b74f97
Thomas Borg Salling <tbsalling@tbsalling.dk>
2018-11-08 12:41:59 +0100

    Set assumed values of own vessel's size

--- near-miss-engine/src/main/java/dk/dma/nearmiss/engine/OwnVessel.java
+++ near-miss-engine/src/main/java/dk/dma/nearmiss/engine/OwnVessel.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OwnVessel extends Vessel {
+
+    {
+        setLoa(100); // TODO make LOA of own vessel configurable
+        setBeam(25); // TODO make beam of own vessel configurable
+    }
+
     public OwnVessel() {
         super(0); // TODO make own mmsi configurable
     }
