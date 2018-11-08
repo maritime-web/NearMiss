9ef2280cd173bddd1d807cae4bc3bd7da26e5c06
Thomas Borg Salling <tbsalling@tbsalling.dk>
2018-11-08 12:42:30 +0100

    Handle situations with incomplete/unavailable data

--- near-miss-engine/src/main/java/dk/dma/nearmiss/engine/engineParts/EllipseShapedSafetyZoneDetector.java
+++ near-miss-engine/src/main/java/dk/dma/nearmiss/engine/engineParts/EllipseShapedSafetyZoneDetector.java
@@ -25,32 +25,65 @@ public class EllipseShapedSafetyZoneDetector implements NearMissDetector {
         Ellipse ellipticContourOfOtherVessel = createContour(otherVessel);
         Ellipse safetyZoneOfOwnVessel = createSafetyZone(ownVessel);
 
-        if (logger.isDebugEnabled()) {
-            logger.debug("WKT contour of other vessel: {}", ellipticContourOfOtherVessel.toWkt());
-            logger.debug("WKT safety zone of own vessel: {}", safetyZoneOfOwnVessel.toWkt());
-        }
+        if (ellipticContourOfOtherVessel != null && safetyZoneOfOwnVessel != null) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("WKT contour of other vessel: {}", ellipticContourOfOtherVessel.toWkt());
+                logger.debug("WKT safety zone of own vessel: {}", safetyZoneOfOwnVessel.toWkt());
+            }
 
-        return safetyZoneOfOwnVessel.intersects(ellipticContourOfOtherVessel);
+            return safetyZoneOfOwnVessel.intersects(ellipticContourOfOtherVessel);
+        } else
+            return false;
     }
 
     private Ellipse createContour(Vessel vessel) {
+        Ellipse contour = null;
+
         double centreLatitude = vessel.getCenterPosition().getLat();
         double centreLongitude = vessel.getCenterPosition().getLon();
-        double lengthOfAxisAlongCourse = vessel.getLoa();
-        double lengthOfAxisAcrossCourse = vessel.getBeam();
-        double course = vessel.getCog();
+        int loa = vessel.getLoa();
+        int beam = vessel.getBeam();
+        double cog = vessel.getCog();
 
-        return new Ellipse(centreLatitude, centreLongitude, lengthOfAxisAlongCourse, lengthOfAxisAcrossCourse, course);
+        if (loa == 0) {
+            logger.debug("LOA of vessel {} is unknown. Cannot calculate safety zone.", vessel.getMmsi());
+        } else if (beam == 0) {
+            logger.debug("Beam of vessel {} is unknown. Cannot calculate safety zone.", vessel.getMmsi());
+        } else if (Double.isNaN(cog)) {
+            logger.debug("COG of vessel {} is unknown. Cannot calculate safety zone.", vessel.getMmsi());
+        } else {
+            contour = new Ellipse(centreLatitude, centreLongitude, loa, beam, cog);
+        }
+
+        return contour;
     }
 
     private Ellipse createSafetyZone(Vessel vessel) {
-        double centreLatitude = vessel.getCenterPosition().getLat();             // TODO offset forward relative to vessel
-        double centreLongitude = vessel.getCenterPosition().getLon();            // TODO offset forward relative to vessel
-        double lengthOfAxisAlongCourse = vessel.getLoa() + vessel.getSog() * 10; // TODO make factor configurable; linear relation to sog? loa?
-        double lengthOfAxisAcrossCourse = vessel.getBeam() * 4;                  // TODO make factor configurable;
-        double course = vessel.getCog();
+        Ellipse safetyZone = null;
 
-        return new Ellipse(centreLatitude, centreLongitude, lengthOfAxisAlongCourse, lengthOfAxisAcrossCourse, course);
+        double sog = vessel.getSog();
+        double cog = vessel.getCog();
+        int loa = vessel.getLoa();
+        int beam = vessel.getBeam();
+
+        if (Double.isNaN(sog)) {
+            logger.debug("SOG of vessel {} is unknown. Cannot calculate safety zone.", vessel.getMmsi());
+        } else if (Double.isNaN(cog)) {
+            logger.debug("COG of vessel {} is unknown. Cannot calculate safety zone.", vessel.getMmsi());
+        } else if (loa == 0) {
+            logger.debug("LOA of vessel {} is unknown. Cannot calculate safety zone.", vessel.getMmsi());
+        } else if (beam == 0) {
+            logger.debug("Beam of vessel {} is unknown. Cannot calculate safety zone.", vessel.getMmsi());
+        } else {
+            double centreLatitude = vessel.getCenterPosition().getLat();  // TODO offset forward relative to vessel
+            double centreLongitude = vessel.getCenterPosition().getLon(); // TODO offset forward relative to vessel
+            int lengthOfAxisAlongCourse = loa + ((int) sog * 10);         // TODO make factor configurable; linear relation to sog? loa?
+            int lengthOfAxisAcrossCourse = beam * 4;                      // TODO make factor configurable;
+
+            safetyZone = new Ellipse(centreLatitude, centreLongitude, lengthOfAxisAlongCourse, lengthOfAxisAcrossCourse, cog);
+        }
+
+        return safetyZone;
     }
 
 }
