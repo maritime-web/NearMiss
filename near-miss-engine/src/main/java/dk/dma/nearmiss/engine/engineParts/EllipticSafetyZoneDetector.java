a735f041d3704b8b511cc4a9b65a3876af575029
Thomas Borg Salling <tbsalling@tbsalling.dk>
2018-11-15 13:27:14 +0100

    Added unit tests for createSafetyZone and createVesselContour

--- near-miss-engine/src/main/java/dk/dma/nearmiss/engine/engineParts/EllipticSafetyZoneDetector.java
+++ near-miss-engine/src/main/java/dk/dma/nearmiss/engine/engineParts/EllipticSafetyZoneDetector.java
@@ -4,6 +4,7 @@ import dk.dma.nearmiss.engine.Vessel;
 import dk.dma.nearmiss.engine.geometry.GeometryService;
 import dk.dma.nearmiss.engine.geometry.geometries.EllipticSafetyZone;
 import dk.dma.nearmiss.engine.geometry.geometries.VesselContour;
+import dk.dma.nearmiss.helper.Position;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -39,7 +40,7 @@ public class EllipticSafetyZoneDetector implements NearMissDetector {
             return false;
     }
 
-    private VesselContour createContour(Vessel vessel) {
+    VesselContour createContour(Vessel vessel) {
         VesselContour contour = null;
 
         double centreLatitude = vessel.getCenterPosition().getLat();
@@ -59,7 +60,7 @@ public class EllipticSafetyZoneDetector implements NearMissDetector {
         return contour;
     }
 
-    private EllipticSafetyZone createSafetyZone(Vessel vessel) {
+    EllipticSafetyZone createSafetyZone(Vessel vessel) {
         EllipticSafetyZone safetyZone = null;
 
         double sog = vessel.getSog();
@@ -76,10 +77,18 @@ public class EllipticSafetyZoneDetector implements NearMissDetector {
         } else if (beam == 0) {
             logger.debug("Beam of vessel {} is unknown. Cannot calculate safety zone.", vessel.getMmsi());
         } else {
-            double centreLatitude = vessel.getCenterPosition().getLat();  // TODO offset forward relative to vessel
-            double centreLongitude = vessel.getCenterPosition().getLon(); // TODO offset forward relative to vessel
-            int lengthOfAxisAlongCourse = loa + ((int) sog * 10);         // TODO make factor configurable; linear relation to sog? loa?
-            int lengthOfAxisAcrossCourse = beam * 4;                      // TODO make factor configurable;
+            // TODO confirm with domain expert that chosen constants are reasonably or correct
+            final double a = 1.2d;
+            final double b = 20d;
+            final double c = 4d;
+            final double d = 3d;
+
+            int lengthOfAxisAlongCourse = (int) ((loa * a + sog * b));
+            int lengthOfAxisAcrossCourse = (int) (beam * c);
+
+            Position ellipticCentre = geometryService.translate(vessel.getCenterPosition(), vessel.getCog(), vessel.getLoa() * vessel.getSog() / 50d + vessel.getSog() * d);
+            double centreLatitude = ellipticCentre.getLat();
+            double centreLongitude = ellipticCentre.getLon();
 
             safetyZone = geometryService.createEllipticSafetyZone(centreLatitude, centreLongitude, lengthOfAxisAlongCourse, lengthOfAxisAcrossCourse, cog);
 
